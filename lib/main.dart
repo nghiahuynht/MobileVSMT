40b53d56ebfe8852820ca8147251752778cb75b5
Thai Ngan <truongthaingan19@gmail.com>
2025-03-12 17:05:48 +0700

    feat: add go_router

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_boilerplate/presentation/my_app.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 
 void main() {
+  /// This is required to remove the '#' from the URL for web
+  /// Before flutterexample.dev/#/path/to/screen
+  /// After flutterexample.dev/path/to/screen
+  usePathUrlStrategy();
+
   runApp(const MyApp());
 }
--- lib/utils/routes/routes.dart
+++ lib/utils/routes/routes.dart
@@ -1,17 +1,17 @@
-// import 'package:boilerplate/presentation/home/home.dart';
-// import 'package:boilerplate/presentation/login/login.dart';
-// import 'package:flutter/material.dart';
+import 'package:flutter_boilerplate/presentation/home/home_screen.dart';
+import 'package:flutter_boilerplate/presentation/login/login_screen.dart';
+import 'package:go_router/go_router.dart';
 
-// class Routes {
-//   Routes._();
-
-//   //static variables
-//   static const String splash = '/splash';
-//   static const String login = '/login';
-//   static const String home = '/post';
-
-//   static final routes = <String, WidgetBuilder>{
-//     login: (BuildContext context) => LoginScreen(),
-//     home: (BuildContext context) => HomeScreen(),
-//   };
-// }
+// GoRouter configuration
+final router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const MyHomePage(),
+    ),
+    GoRoute(
+      path: '/login',
+      builder: (context, state) => const LoginScreen(),
+    ),
+  ],
+);
